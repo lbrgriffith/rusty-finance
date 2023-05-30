61022a845de221741dbd0cd0503ecbc8acd2259e
Ricardo Griffith <ricardo.griffith@icloud.com>
2023-05-30 16:28:29 -0300

    added function for simple interest calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use clap::{Parser};
+
+/// Financial calculation tool
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Opts {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Parser, Debug)]
+enum Command {
+    /// Calculates simple interest
+    Interest(Interest),
+}
+
+#[derive(Parser, Debug)]
+struct Interest {
+    /// The principal amount (initial money)
+    #[clap(short, long)]
+    principal: f64,
+
+    /// The rate of interest (per period)
+    #[clap(short, long)]
+    rate: f64,
+
+    /// The time the money is invested for
+    #[clap(short, long)]
+    time: f64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    match opts.command {
+        Command::Interest(interest) => {
+            let Interest { principal, rate, time } = interest;
+            let result = principal * rate * time;
+            println!("The simple interest is: {}", result);
+        }
+    }
 }
