13e4d7014f06c0e8347856f6331325f964ace46c
Ricardo Griffith <ricardo.griffith@icloud.com>
2023-05-30 17:29:22 -0300

    extended to include compound interest

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Parser};
+use clap::Parser;
 
 /// Financial calculation tool
 #[derive(Parser, Debug)]
@@ -12,6 +12,8 @@ struct Opts {
 enum Command {
     /// Calculates simple interest
     Interest(Interest),
+    /// Calculates compound interest
+    CompoundInterest(CompoundInterest),
 }
 
 #[derive(Parser, Debug)]
@@ -29,6 +31,25 @@ struct Interest {
     time: f64,
 }
 
+#[derive(Parser, Debug)]
+struct CompoundInterest {
+    /// The principal amount (initial money)
+    #[clap(short, long)]
+    principal: f64,
+
+    /// The annual interest rate
+    #[clap(short, long)]
+    rate: f64,
+
+    /// The number of times interest is compounded per year
+    #[clap(short, long)]
+    n: i32,
+
+    /// The time the money is invested for in years
+    #[clap(short, long)]
+    t: i32,
+}
+
 fn main() {
     let opts: Opts = Opts::parse();
 
@@ -37,6 +58,14 @@ fn main() {
             let Interest { principal, rate, time } = interest;
             let result = principal * rate * time;
             println!("The simple interest is: {}", result);
+        }
+        Command::CompoundInterest(compound_interest) => {
+            let CompoundInterest { principal, rate, n, t } = compound_interest;
+            let mut amount = principal;
+            for year in 1..=t {
+                amount *= 1.0 + (rate / n as f64);
+                println!("Year {}: {}", year, amount);
+            }
         }
     }
 }
